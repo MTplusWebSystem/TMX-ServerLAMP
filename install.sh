60edf24dff078bc8bfc99c7afddecb1f292b73c0
MTplusWebSystem <mtplusewbsystem@gmail.com>
2024-03-15 13:22:19 -0400

    v0.0.1

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ rm -rf /data/data/com.termux/files/usr/etc/apache2/httpd.conf
 
 cp httpd.conf /data/data/com.termux/files/usr/etc/apache2
 
+rm -rf /data/data/com.termux/files/usr/share/phpmyadmin/config.inc.php
+
+cp config.inc.php /data/data/com.termux/files/usr/share/phpmyadmin
 
 echo "Iniciar phpMyadmin"
 
